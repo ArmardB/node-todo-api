1f89b1d9649c3cb3771336b6ad37de120506f18b
Armard Bellamy <Armard@Armards-MacBook.local>
2017-11-24 13:24:10 -0500

    Set up post route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,20 @@ const { User } = require('./models/user');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc)=> {
+    res.send(doc);
+  },(err)=> {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000, ()=> {
   console.log('Listening on port 3000');
 });
