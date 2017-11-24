6a429dabc8c4b29a1565bd975203067bb50b76a7
Armard Bellamy <Armard@Armards-MacBook.local>
2017-11-24 15:58:47 -0500

    Added GET /users/:id and /todos/:id routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose} = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -31,7 +32,39 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
 
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send({});
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+      return res.status(200).send({todo});
+  }).catch((err) =>{
+    res.status(404).send();
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send({});
+  }
+
+  User.findById(id).then((user) => {
+    if (!user) {
+      return res.status(404).send();
+    }
+      return res.status(200).send({user});
+  }).catch((err) =>{
+    res.status(404).send();
+  });
+});
 
 app.listen(3000, ()=> {
   console.log('Listening on port 3000');
